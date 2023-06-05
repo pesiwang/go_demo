da8c344c11e3d1fe9f05e7335d5784923c00e381
wanglei8 <wanglei8@xunlei.com>
2023-06-05 18:11:35 +0800

    update gin_sample

--- gin_sample/main.go
+++ gin_sample/main.go
@@ -8,27 +8,64 @@ import (
 )
 
 // album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
+type Album struct {
+	ID     string  `json:"id" binding:"min=1,max=6"`
+	Title  string  `json:"title" binding:"min=1,max=9999"`
 	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	Price  float64 `json:"price" binding:"required,gt=0,lte=99"`
+}
+
+type AlbumQuery struct {
+	ID string `form:"id" binding:"required,min=1,max=6"`
 }
 
 // albums slice to seed record album data.
-var albums = []album{
+var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+func globalMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("middleware globalMiddleware begin --------")
+		c.Next()
+		fmt.Println("middleware globalMiddleware end --------")
+	}
+}
+
+func userMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("middleware user begin ----")
+		c.Next()
+		fmt.Println("middleware user end ----")
+	}
+}
+
+func getUserInfo(c *gin.Context) {
+	fmt.Printf("recv /user/get_info get request\n")
+
+	c.JSON(http.StatusOK, gin.H{"name": "wolf", "age": 18})
+}
+
+func getUserStatus(c *gin.Context) {
+	fmt.Printf("recv /user/get_status get request\n")
+
+	c.JSON(http.StatusOK, gin.H{"status": "running"})
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.Use(globalMiddleware())
+	router.GET("/album_list", getAlbums)
+	router.GET("/album_query", getAlbumByID)
+	router.POST("/album_add", postAlbums)
 
-	router.Run("localhost:8080")
+	userGroup := router.Group("/user")
+	userGroup.GET("/get_status", getUserStatus)
+	userGroup.Use(userMiddleware())
+	userGroup.GET("/get_info", getUserInfo)
+	router.Run("localhost:8081")
 }
 
 // getAlbums responds with the list of all albums as JSON.
@@ -40,11 +77,13 @@ func getAlbums(c *gin.Context) {
 
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
-	var newAlbum album
+	var newAlbum Album
 
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err := c.ShouldBindJSON(&newAlbum); err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusOK, gin.H{"message": "params not valid!"})
 		return
 	}
 
@@ -56,7 +95,16 @@ func postAlbums(c *gin.Context) {
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
+	var r AlbumQuery
+	err := c.ShouldBindQuery(&r)
+	if err != nil {
+		fmt.Printf("ShouldBindQuery error:%s", err)
+		c.JSON(http.StatusOK, gin.H{"message": "id not valid!"})
+		return
+	}
+	id := r.ID
+
+	fmt.Printf("recv id %v", r.ID)
 
 	// Loop through the list of albums, looking for
 	// an album whose ID value matches the parameter.
