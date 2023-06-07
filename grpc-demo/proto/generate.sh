8dd438bef338c79db92a3b1899c1d81b848771c3
wanglei8 <wanglei8@xunlei.com>
2023-06-07 21:56:28 +0800

    support grpc gateway for http service

--- grpc-demo/proto/generate.sh
+++ grpc-demo/proto/generate.sh
@@ -1,6 +1,8 @@
 
 # run in proto directory
-protoc -I . -I ..\..\..\protobuf\src\ --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative,require_unimplemented_servers=false common/common.proto
+protoc -I . -I ..\..\..\protobuf\src\ -I ..\..\..\googleapis\ --go_out . --go_opt paths=source_relative --go-grpc_out . --grpc-gateway_out .  --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --go-grpc_opt paths=source_relative,require_unimplemented_servers=false common/common.proto
 
 
-protoc -I .  -I ..\..\..\protobuf\src\ --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative,require_unimplemented_servers=false bizdemo/bizdemo.proto
+
+
+protoc -I .  -I ..\..\..\protobuf\src\ -I ..\..\..\googleapis\ --go_out=. --go_opt paths=source_relative --go-grpc_out . --grpc-gateway_out . --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --go-grpc_opt paths=source_relative,require_unimplemented_servers=false bizdemo/bizdemo.proto
--- grpc-demo/server/server.go
+++ grpc-demo/server/server.go
@@ -9,18 +9,39 @@ protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=p
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"proto/bizdemo"
 	"server/interceptor"
 	"server/service"
 
 	gpm "github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
 const grpcPort = 6655
+
+func startHttpServer(grpcServer *grpc.Server) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := bizdemo.RegisterBizDemoHandlerFromEndpoint(ctx, mux, "localhost:6655", opts)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("http server is listening....")
+
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	return http.ListenAndServe(":8081", mux)
+}
 
 func main() {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
@@ -37,6 +58,8 @@ func main() {
 			),
 		),
 	)
+
+	go startHttpServer(grpcServer)
 
 	bizdemo.RegisterBizDemoServer(grpcServer, &service.BizDemo{})
 
