4a0e40e3aa5b6c7df60aea16f9bf42849fb82de1
wanglei8 <wanglei8@xunlei.com>
2023-06-21 19:37:16 +0800

    add panic_recover

---
+++ panic_recover/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func set_data(x int) {
+	// defer func() {
+	// 	// recover() 可以将捕获到的panic信息打印
+	// 	if err := recover(); err != nil {
+	// 		panicErrMsg := fmt.Sprintf("panic because: %v\n", err)
+	// 		stack := "this is set_data() panic stack"
+	// 		msg := panicErrMsg + stack
+	// 		fmt.Println(msg)
+	// 	}
+	// }()
+
+	// 故意制造数组越界，触发 panic
+	var arr [10]int
+	arr[x] = 88
+}
+
+func main() {
+
+	defer func() {
+		// 无法捕获其他 goroutine 触发的 panic
+		if err := recover(); err != nil {
+			panicErrMsg := fmt.Sprintf("panic because: %v\n", err)
+			stack := "this is main goroutine panic stack"
+			msg := panicErrMsg + stack
+			fmt.Println(msg)
+		}
+	}()
+
+	panic("this is panic msg")
+
+	// go func() {
+	// 	set_data(27)
+	// }()
+
+	// time.Sleep(time.Second * 3)
+
+	fmt.Println("main goroutine quit")
+}
