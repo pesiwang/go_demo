2878baf9ef8c23fab88a35f10a95f72491335980
wanglei8 <wanglei8@xunlei.com>
2023-11-06 21:25:22 +0800

    crontab

--- cron_sample/go.mod
+++ cron_sample/go.mod
@@ -2,4 +2,4 @@ module cron_sample
 
 go 1.20
 
-require github.com/robfig/cron v1.2.0 // indirect
+require github.com/robfig/cron/v3 v3.0.1
--- cron_sample/main.go
+++ cron_sample/main.go
@@ -4,22 +4,31 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/robfig/cron"
+	"github.com/robfig/cron/v3"
 )
 
 func print() {
-	fmt.Println(time.Now().Unix())
+	fmt.Println(time.Now())
 }
 
+func TriggerNewUserAwakeDaily() {
+	fmt.Println("TriggerNewUserAwakeDaily", time.Now().Unix())
+}
 func main() {
-	loc, _ := time.LoadLocation("Asia/Shanghai")
-	crontab := cron.NewWithLocation(loc)
+	cronMain := cron.New(cron.WithSeconds())
 
-	err := crontab.AddFunc("*/2 * * * * *", print)
-	crontab.Start()
+	_, err := cronMain.AddFunc("*/2 * * * * *", print)
 	if err != nil {
 		fmt.Printf("crontab.AddFunc err %v\n", err)
 	}
+
+	_, err = cronMain.AddFunc("0 16 20 * * ?", TriggerNewUserAwakeDaily) // 每天晚上 20 点开始触发新用户唤醒
+	if err != nil {
+		fmt.Printf("cronMain.AddFunc err %v\n", err)
+	}
+
+	cronMain.Start()
+	fmt.Printf("started\n")
 
 	select {}
 }
